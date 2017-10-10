179813e82f9576deb8d07f64c264163e1efa3843
damienbod <damien_bod@hotmail.com>
2017-10-10 18:28:17 +0200

    code clean up, oidc client config

--- SecurityAspNetCoreSignalR/angularApp/app/app.module.ts
+++ SecurityAspNetCoreSignalR/angularApp/app/app.module.ts
@@ -54,26 +54,32 @@ export class AppModule {
         console.log('APP STARTING');
         this.configClient().subscribe(config => {
 
-            console.log(this.clientConfiguration);
-            const openIDImplicitFlowConfiguration = new OpenIDImplicitFlowConfiguration();
-            openIDImplicitFlowConfiguration.stsServer = this.clientConfiguration.urlStsServer;
-
-            openIDImplicitFlowConfiguration.redirect_url = this.clientConfiguration.urlRedirect;
-            openIDImplicitFlowConfiguration.client_id = 'angularclient';
-            openIDImplicitFlowConfiguration.response_type = 'id_token token';
-            openIDImplicitFlowConfiguration.scope = ' openid vmsscope profile email';
-            openIDImplicitFlowConfiguration.post_logout_redirect_uri = this.clientConfiguration.urlRedirectPostLogout;
-            openIDImplicitFlowConfiguration.start_checksession = false;
-            openIDImplicitFlowConfiguration.silent_renew = true;
-            openIDImplicitFlowConfiguration.startup_route = '/calculator';
-            openIDImplicitFlowConfiguration.forbidden_route = '/unauthorized';
-            openIDImplicitFlowConfiguration.unauthorized_route = '/unauthorized';
-            openIDImplicitFlowConfiguration.trigger_authorization_result_event = true;
-            openIDImplicitFlowConfiguration.log_console_warning_active = true;
-            openIDImplicitFlowConfiguration.log_console_debug_active = true;
-            openIDImplicitFlowConfiguration.max_id_token_iat_offset_allowed_in_seconds = 10;
+            let openIDImplicitFlowConfiguration = new OpenIDImplicitFlowConfiguration();
+            openIDImplicitFlowConfiguration.stsServer = this.clientConfiguration.stsServer;
+            openIDImplicitFlowConfiguration.redirect_url = this.clientConfiguration.redirect_url;
+            // The Client MUST validate that the aud (audience) Claim contains its client_id value registered at the Issuer identified by the iss (issuer) Claim as an audience.
+            // The ID Token MUST be rejected if the ID Token does not list the Client as a valid audience, or if it contains additional audiences not trusted by the Client.
+            openIDImplicitFlowConfiguration.client_id = this.clientConfiguration.client_id;
+            openIDImplicitFlowConfiguration.response_type = this.clientConfiguration.response_type;
+            openIDImplicitFlowConfiguration.scope = this.clientConfiguration.scope;
+            openIDImplicitFlowConfiguration.post_logout_redirect_uri = this.clientConfiguration.post_logout_redirect_uri;
+            openIDImplicitFlowConfiguration.start_checksession = this.clientConfiguration.start_checksession;
+            openIDImplicitFlowConfiguration.silent_renew = this.clientConfiguration.silent_renew;
+            openIDImplicitFlowConfiguration.startup_route = this.clientConfiguration.startup_route;
+            // HTTP 403
+            openIDImplicitFlowConfiguration.forbidden_route = this.clientConfiguration.forbidden_route;
+            // HTTP 401
+            openIDImplicitFlowConfiguration.unauthorized_route = this.clientConfiguration.unauthorized_route;
+            openIDImplicitFlowConfiguration.log_console_warning_active = this.clientConfiguration.log_console_warning_active;
+            openIDImplicitFlowConfiguration.log_console_debug_active = this.clientConfiguration.log_console_debug_active;
+            // id_token C8: The iat Claim can be used to reject tokens that were issued too far away from the current time,
+            // limiting the amount of time that nonces need to be stored to prevent attacks.The acceptable range is Client specific.
+            openIDImplicitFlowConfiguration.max_id_token_iat_offset_allowed_in_seconds = this.clientConfiguration.max_id_token_iat_offset_allowed_in_seconds;
 
             this.oidcSecurityService.setupModule(openIDImplicitFlowConfiguration);
+
+            // configuration.FileServer = this.clientConfiguration.apiFileServer;
+            // configuration.Server = this.clientConfiguration.apiServer;
         });
     }
 
