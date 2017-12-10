8ce5408dad35448fafbdd8f8fe0dcfa0d8acda14
damienbod <damien_bod@hotmail.com>
2017-12-10 10:05:36 +0100

    updating rxjs

--- SecurityAspNetCoreSignalR/angularApp/app/news/store/news.effects.ts
+++ SecurityAspNetCoreSignalR/angularApp/app/news/store/news.effects.ts
@@ -1,9 +1,7 @@
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/switchMap';
+import { catchError, map, switchMap } from 'rxjs/operators';
 
 import { Injectable } from '@angular/core';
 import { Actions, Effect } from '@ngrx/effects';
-import { Action } from '@ngrx/store';
 import { of } from 'rxjs/observable/of';
 import { Observable } from 'rxjs/Rx';
 
@@ -13,34 +11,34 @@ import { NewsService } from '../news.service';
 @Injectable()
 export class NewsEffects {
 
-    @Effect() sendNewItem$ = this.actions$.ofType<newsAction.SendNewsItemAction>(newsAction.SEND_NEWS_ITEM)
-        .switchMap((action: newsAction.SendNewsItemAction) => {
+    @Effect() sendNewItem$ = this.actions$.ofType<newsAction.SendNewsItemAction>(newsAction.SEND_NEWS_ITEM).pipe(
+        switchMap((action: newsAction.SendNewsItemAction) => {
             this.newsService.send(action.newsItem);
             return Observable.of(new newsAction.SendNewsItemActionComplete(action.newsItem));
-        });
+        }));
 
-    @Effect() joinGroup$ = this.actions$.ofType<newsAction.JoinGroupAction>(newsAction.JOIN_GROUP)
-        .switchMap((action: newsAction.JoinGroupAction) => {
+    @Effect() joinGroup$ = this.actions$.ofType<newsAction.JoinGroupAction>(newsAction.JOIN_GROUP).pipe(
+        switchMap((action: newsAction.JoinGroupAction) => {
             this.newsService.joinGroup(action.group);
             return Observable.of(new newsAction.JoinGroupActionComplete(action.group));
-        });
+        }));
 
-    @Effect() leaveGroup$: Observable<Action> = this.actions$.ofType<newsAction.LeaveGroupAction>(newsAction.LEAVE_GROUP)
-        .switchMap((action: newsAction.LeaveGroupAction) => {
+    @Effect() leaveGroup$ = this.actions$.ofType<newsAction.LeaveGroupAction>(newsAction.LEAVE_GROUP).pipe(
+        switchMap((action: newsAction.LeaveGroupAction) => {
             this.newsService.leaveGroup(action.group);
             return Observable.of(new newsAction.LeaveGroupActionComplete(action.group));
-        });
+        }));
 
-    @Effect() getAllGroups$ = this.actions$.ofType(newsAction.SELECTALL_GROUPS)
-        .switchMap(() =>
-            this.newsService.getAllGroups()
-                .map((data: string[]) => {
+
+    @Effect() getAllGroups$ = this.actions$.ofType(newsAction.SELECTALL_GROUPS).pipe(
+        switchMap(() => {
+            return this.newsService.getAllGroups().pipe(
+                map((data: string[]) => {
                     return new newsAction.SelectAllGroupsActionComplete(data);
-                })
-                .catch(() => {
-                    return of({ type: 'getAllGroups$' })
-                })
-        );
+                }),
+                catchError((error: any) => of(error)
+                ))
+        }));
 
     constructor(
         private newsService: NewsService,
