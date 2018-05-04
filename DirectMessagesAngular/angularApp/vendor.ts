19ac6698173ef4328a08b6a140def3122af22dfe
damienbod <damien_bod@hotmail.com>
2018-05-04 11:24:10 +0200

    updating to Angular 6

--- DirectMessagesAngular/angularApp/vendor.ts
+++ DirectMessagesAngular/angularApp/vendor.ts
@@ -1,2 +1,9 @@
-﻿// import 'jquery/src/jquery';
+﻿import 'jquery/dist/jquery';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import '../favicon.ico';
+
+// Other vendors for example jQuery, Lodash or Bootstrap
+// You can import js, ts, css, sass, ...
 
