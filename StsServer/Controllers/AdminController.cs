0b85376a8624e5148f198da3a0453d01689ec9af
damienbod <damien_bod@hotmail.com>
2018-10-30 13:34:02 +0100

    code clean up

--- StsServer/Controllers/AdminController.cs
+++ StsServer/Controllers/AdminController.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using StsServer.Data;
 using StsServer.Models;
