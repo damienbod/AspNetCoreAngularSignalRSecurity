f12b46b900b131efd19d5291b13ce88f2b1a28f5
damienbod <damien_bod@hotmail.com>
2018-10-30 09:32:37 +0100

    Adding the policies

---
+++ StsServer/Policies/IsAdminHandler.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StsServer
+{
+    public class IsAdminHandler : AuthorizationHandler<IsAdminRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, IsAdminRequirement requirement)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            if (requirement == null)
+                throw new ArgumentNullException(nameof(requirement));
+
+            var adminClaim = context.User.Claims.FirstOrDefault(t => t.Value == "admin" && t.Type == "evaluation_tool_role"); 
+            if (adminClaim != null)
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ StsServer/Policies/IsAdminRequirement.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.AspNetCore.Authorization;
+ 
+namespace StsServer
+{
+    public class IsAdminRequirement : IAuthorizationRequirement{}
+}
