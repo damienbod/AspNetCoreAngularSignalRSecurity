c629d119252b9e0950f434fb91e45f7884adebee
damienbod <damien_bod@hotmail.com>
2020-05-01 17:29:39 +0200

    updating oidc lib

--- DirectMessagesAngular/angularApp/app/app.module.ts
+++ DirectMessagesAngular/angularApp/app/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { Configuration } from './app.constants';
 import { routing } from './app.routes';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
 
@@ -19,17 +19,26 @@ import {MatButtonModule} from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {
-    AuthModule,
-    OidcSecurityService,
-    ConfigResult,
-    OidcConfigService,
-    OpenIdConfiguration
-} from 'angular-auth-oidc-client';
+import { AuthModule, OidcConfigService } from 'angular-auth-oidc-client';
 
-export function loadConfig(oidcConfigService: OidcConfigService) {
-    console.log('APP_INITIALIZER STARTING');
-    return () => oidcConfigService.load_using_stsServer('https://localhost:44318');
+export function configureAuth(oidcConfigService: OidcConfigService) {
+    return () =>
+        oidcConfigService.withConfig({
+            stsServer: 'https://localhost:44318',
+            redirectUrl: window.location.origin,
+            postLogoutRedirectUri: 'https://localhost:44395/unauthorized',
+            clientId: 'angularclient2',
+            scope: 'dataEventRecords openid profile email',
+            responseType: 'code',
+            silentRenew: true,
+            silentRenewUrl: `${window.location.origin}/silent-renew.html`,
+            renewTimeBeforeTokenExpiresInSeconds: 10,
+            logLevel: LogLevel.Debug,
+			postLoginRoute: '/dm',
+			forbiddenRoute: '/unauthorized',
+            unauthorizedRoute: '/unauthorized',
+            historyCleanupOff: true
+        });
 }
 
 @NgModule({
@@ -56,50 +65,16 @@ export function loadConfig(oidcConfigService: OidcConfigService) {
         UnauthorizedComponent
     ],
     providers: [
-        OidcSecurityService,
         OidcConfigService,
         {
             provide: APP_INITIALIZER,
-            useFactory: loadConfig,
-            deps: [OidcConfigService],
-            multi: true
+            useFactory: configureAuth,
+            deps: [OidcConfigService, HttpClient],
+            multi: true,
         },
         Configuration
     ],
     bootstrap:    [AppComponent],
 })
 
-export class AppModule {
-    constructor(
-        private oidcSecurityService: OidcSecurityService,
-        private oidcConfigService: OidcConfigService,
-    ) {
-        this.oidcConfigService.onConfigurationLoaded.subscribe((configResult: ConfigResult) => {
-
-            const config: OpenIdConfiguration = {
-                stsServer: 'https://localhost:44318',
-                redirect_url: 'https://localhost:44395',
-                client_id: 'angularclient2',
-                response_type: 'code',
-                scope: 'dataEventRecords openid profile email',
-                post_logout_redirect_uri: 'https://localhost:44395/unauthorized',
-                start_checksession: false,
-                silent_renew: true,
-                silent_renew_url: 'https://localhost:44395/silent-renew.html',
-                post_login_route: '/dm',
-                forbidden_route: '/unauthorized',
-                unauthorized_route: '/unauthorized',
-                log_console_warning_active: true,
-                log_console_debug_active: false,
-                max_id_token_iat_offset_allowed_in_seconds: 10,
-                history_cleanup_off: true
-                // iss_validation_off: false
-                // disable_iat_offset_validation: true
-            };
-
-            this.oidcSecurityService.setupModule(config, configResult.authWellknownEndpoints);
-        });
-
-        console.log('APP STARTING');
-    }
-}
+export class AppModule {}
