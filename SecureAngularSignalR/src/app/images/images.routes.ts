9d2fc96fc7e8e1ceb4735f0665c28f595e0c5e26
damienbod <damien_bod@hotmail.com>
2021-01-23 18:48:24 +0100

    removing unused module

--- SecureAngularSignalR/src/app/images/images.routes.ts
+++
@@ -1,7 +0,0 @@
-import { RouterModule, Routes } from '@angular/router';
-
-import { ImagesComponent } from './components/images.component';
-
-const routes: Routes = [{ path: 'images', component: ImagesComponent }];
-
-export const ImagesRoutes = RouterModule.forChild(routes);
