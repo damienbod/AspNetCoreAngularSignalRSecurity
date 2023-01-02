e14c58b554f4b7f84793a04745f57976f5d12944
damienbod <damien_bod@hotmail.com>
2023-01-02 21:28:37 +0100

    code clean up

--- ApiServer/Controllers/DataEventRecordController.cs
+++ ApiServer/Controllers/DataEventRecordController.cs
@@ -1,5 +1,6 @@
 using ApiServer.Model;
 using ApiServer.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
