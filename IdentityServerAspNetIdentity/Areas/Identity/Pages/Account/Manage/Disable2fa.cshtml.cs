0fbdd30f3ff53ab9d8a15296689699d3fa8a2caa
damienbod <damien_bod@hotmail.com>
2023-01-06 23:02:41 +0100

    Update FIDO2 settings

--- IdentityServerAspNetIdentity/Areas/Identity/Pages/Account/Manage/Disable2fa.cshtml.cs
+++ IdentityServerAspNetIdentity/Areas/Identity/Pages/Account/Manage/Disable2fa.cshtml.cs
@@ -14,15 +14,22 @@ namespace IdentityServerAspNetIdentity.Areas.Identity.Pages.Account.Manage
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly ILogger<Disable2faModel> _logger;
+        private readonly Fido2Store _fido2Store;
 
         public Disable2faModel(
-            UserManager<ApplicationUser> userManager,
+            UserManager<IdentityUser> userManager,
+            Fido2Store fido2Store,
             ILogger<Disable2faModel> logger)
         {
             _userManager = userManager;
+            _fido2Store = fido2Store;
             _logger = logger;
         }
 
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
         [TempData]
         public string StatusMessage { get; set; }
 
@@ -36,7 +43,7 @@ namespace IdentityServerAspNetIdentity.Areas.Identity.Pages.Account.Manage
 
             if (!await _userManager.GetTwoFactorEnabledAsync(user))
             {
-                throw new InvalidOperationException($"Cannot disable 2FA for user with ID '{_userManager.GetUserId(User)}' as it's not currently enabled.");
+                throw new InvalidOperationException($"Cannot disable 2FA for user as it's not currently enabled.");
             }
 
             return Page();
@@ -50,10 +57,13 @@ namespace IdentityServerAspNetIdentity.Areas.Identity.Pages.Account.Manage
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
 
+            // remove Fido2 MFA if it exists
+            await _fido2Store.RemoveCredentialsByUserNameAsync(user.UserName);
+
             var disable2faResult = await _userManager.SetTwoFactorEnabledAsync(user, false);
             if (!disable2faResult.Succeeded)
             {
-                throw new InvalidOperationException($"Unexpected error occurred disabling 2FA for user with ID '{_userManager.GetUserId(User)}'.");
+                throw new InvalidOperationException($"Unexpected error occurred disabling 2FA.");
             }
 
             _logger.LogInformation("User with ID '{UserId}' has disabled 2fa.", _userManager.GetUserId(User));
--- IdentityServerAspNetIdentity/Areas/Identity/Pages/Account/Manage/TwoFactorAuthentication.cshtml.cs
+++ IdentityServerAspNetIdentity/Areas/Identity/Pages/Account/Manage/TwoFactorAuthentication.cshtml.cs
@@ -12,35 +12,49 @@ namespace IdentityServerAspNetIdentity.Areas.Identity.Pages.Account.Manage
 {
     public class TwoFactorAuthenticationModel : PageModel
     {
-        private const string AuthenicatorUriFormat = "otpauth://totp/{0}:{1}?secret={2}&issuer={0}";
-
-        private readonly UserManager<ApplicationUser> _userManager;
-        private readonly SignInManager<ApplicationUser> _signInManager;
-        private readonly ILogger<TwoFactorAuthenticationModel> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
 
         public TwoFactorAuthenticationModel(
-            UserManager<ApplicationUser> userManager,
-            SignInManager<ApplicationUser> signInManager,
-            ILogger<TwoFactorAuthenticationModel> logger)
+            UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
-            _logger = logger;
         }
 
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
         public bool HasAuthenticator { get; set; }
 
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
         public int RecoveryCodesLeft { get; set; }
 
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
         [BindProperty]
         public bool Is2faEnabled { get; set; }
 
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
         public bool IsMachineRemembered { get; set; }
 
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
         [TempData]
         public string StatusMessage { get; set; }
 
-        public async Task<IActionResult> OnGet()
+        public async Task<IActionResult> OnGetAsync()
         {
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
@@ -56,7 +70,7 @@ namespace IdentityServerAspNetIdentity.Areas.Identity.Pages.Account.Manage
             return Page();
         }
 
-        public async Task<IActionResult> OnPost()
+        public async Task<IActionResult> OnPostAsync()
         {
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
